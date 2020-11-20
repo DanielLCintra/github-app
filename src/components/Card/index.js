66f77af6c67275f72c944a1e32942b260e19e8fb
Daniel Cintra <cintra.70@gmail.com>
2020-11-20 11:57:59 -0300

    Atualizações

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./styles.scss";
 
 const Card = ({ children }) => {
-  return <div className="card">{children}</div>;
+  return (
+    <div className="card-container">
+      <div className="card">{children}</div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ const Home = () => {
         iconSize="lg"
       />
 
-      <Card>aaa</Card>
+      <Card />
     </>
   );
 };
