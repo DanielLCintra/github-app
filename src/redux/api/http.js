ca1865e0b91dae0a624113dfebabdb4e7fe1de8e
Daniel Cintra <cintra.70@gmail.com>
2020-11-25 14:52:49 -0300

    Remover credenciais

--- src/redux/api/http.js
+++ src/redux/api/http.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-var username = "cintra.70@gmail.com";
-var password = "sp21vc642013";
-var credentials = btoa(username + ":" + password);
-var basicAuth = "Basic " + credentials;
-
 const http = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: { Authorization: +basicAuth },
 });
 
 export default http;
